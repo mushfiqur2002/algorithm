b0e6c8b253bbb54a8704853ebab058f54ec0d979
mushfiqur2002 <mushfiqurnasim@gmail.com>
2025-09-15 22:39:05 +0600

    add sort funciton

--- insertionsort.js
+++ insertionsort.js
@@ -10,13 +10,14 @@ for (let i = 0; i < 10; i++) {
     arr2.push(randNum);
 }
 console.log(arr2);
+console.log('sort function: ', arr2.sort((a, b) => b - a));
 
 
 function insertionSort(array) {
     for (let i = 1; i < array.length; i++) {
         let k = array[i];
         let j = i - 1;
-        while (j >= 0 && array[j] > k) {
+        while (j >= 0 && array[j] < k) {
             array[j + 1] = array[j];
             j--;
         }
